5411bb460d936943c29a32e889a459076d1787c9
Aksh444 <aktcool4@gmail.com>
2025-07-22 20:07:50 +0530

    First Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logomain} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>CareSlot — Your trusted partner in healthcare. Book appointments effortlessly, connect with top doctors across specialties, and take charge of your well-being. Convenient, reliable, and patient-first — because your health deserves the best care.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91-1234567809</li>
+            <li>careslot@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ CareSlot.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
